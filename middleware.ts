96760470f217e76f8891934a8cb21873488ce4e0
vansh845 <103567533+vansh845@users.noreply.github.com>
2024-01-16 03:06:54 +0530

    heeh

--- middleware.ts
+++ middleware.ts
@@ -4,29 +4,7 @@ import type { NextRequest } from 'next/server'
 import { authMiddleware, redirectToSignIn } from '@clerk/nextjs';
 
 // the following code is taken from : https://nextjs.org/docs/advanced-features/middleware#setting-headers
-export default authMiddleware({
-  afterAuth(auth, req, evt) {
-    // Handle users who aren't authenticated
-    if (!auth.userId && !auth.isPublicRoute) {
-      return redirectToSignIn({ returnBackUrl: req.url });
-    }
-    // Redirect logged in users to organization selection page if they are not active in an organization
-    if (
-      auth.userId &&
-      !auth.orgId &&
-      req.nextUrl.pathname !== "/org-selection"
-    ) {
-      const orgSelection = new URL("/org-selection", req.url);
-      return NextResponse.redirect(orgSelection);
-    }
-    // If the user is logged in and trying to access a protected route, allow them to access route
-    if (auth.userId && !auth.isPublicRoute) {
-      return NextResponse.next();
-    }
-    // Allow users visiting public routes to access them
-    return NextResponse.next();
-  },
-});
+
 export function middleware(request: NextRequest) {
 
     const requestHeaders = new Headers(request.headers);
